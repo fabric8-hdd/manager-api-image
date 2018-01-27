9c4ce9eb97b3266ef57d94a38418f73e36f15427
anmolbabu <anmolbudugutta@gmail.com>
2018-01-27 19:45:33 +0530

    Parameterise api prefix
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tests/api_test.js
+++ tests/api_test.js
@@ -1,16 +1,21 @@
+process.env.ROUTE_PREFIX = "/api/v1.0"
+process.env.ROUTE_PATH = "/manager"
+
 var server   = require("../main"),
 	chai     = require("chai"),
 	chaiHTTP = require("chai-http"),
-	should   = chai.should()
+	should   = chai.should(),
+	route_path = process.env.ROUTE_PATH || "/manager",
+	route_prefix = process.env.ROUTE_PREFIX
 
 chai.use(chaiHTTP)
 
 describe("Test manager api", function() {
 
-	describe("GET to /manager/api/v1.0/experiments/22f82a83-7b1b-4556-aeea-5b3b16a9b287", () => {
+	describe("GET to /experiments/22f82a83-7b1b-4556-aeea-5b3b16a9b287", () => {
 		it(" should return 200", function(done){
 			chai.request(server)
-				.get("/manager/api/v1.0/experiments/22f82a83-7b1b-4556-aeea-5b3b16a9b287")
+				.get(process.env.ROUTE_PATH + process.env.ROUTE_PREFIX + "/experiments/22f82a83-7b1b-4556-aeea-5b3b16a9b287")
 				.end(function(err, res) {
 					res.should.have.status(200)
 					done()
@@ -19,10 +24,10 @@ describe("Test manager api", function() {
 	})
 
 
-	describe("GET to /manager/api/v1.0/experiments/xxxx", () => {
+	describe("GET to /experiments/xxxx", () => {
 		it("should return 400", function(done){
 			chai.request(server)
-				.get("/manager/api/v1.0/experiments/xxxx")
+				.get(process.env.ROUTE_PATH + process.env.ROUTE_PREFIX + "/experiments/xxxx")
 				.end(function(err, res) {
 					res.should.have.status(400)
 					done()
@@ -34,10 +39,10 @@ describe("Test manager api", function() {
 
 describe("Test readiness and liveness api",function(){
 
-	describe("Get to /manager/api/v1.0/readiness", () => {
+	describe("Get to /readiness", () => {
 		it(" should return 200", function(done){
 			chai.request(server)
-				.get("/manager/api/v1.0/readiness")
+				.get(process.env.ROUTE_PATH + process.env.ROUTE_PREFIX + "/readiness")
 				.end(function(err, res) {
 					res.should.have.status(200)
 					done()
@@ -45,10 +50,10 @@ describe("Test readiness and liveness api",function(){
 		})
 	})
 
-	describe("Get to /manager/api/v1.0/liveness", () => {
+	describe("Get to /liveness", () => {
 		it(" should return 200", function(done){
 			chai.request(server)
-				.get("/manager/api/v1.0/liveness")
+				.get(process.env.ROUTE_PATH + process.env.ROUTE_PREFIX + "/liveness")
 				.end(function(err, res) {
 					res.should.have.status(200)
 					done()
@@ -56,4 +61,3 @@ describe("Test readiness and liveness api",function(){
 		})
 	})
 })
-
