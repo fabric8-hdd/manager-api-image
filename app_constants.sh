0349d4ee95be357c6086885590fe1b65a4185d46
anmolbabu <anmolbudugutta@gmail.com>
2018-02-06 13:08:55 +0530

    Rename manager-api-image to fabric8-hypothesis-manager-api-image
    fabric8-hypothesis-bug-id: fabric8-hdd/fabric8-hypothesis-planning#4
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- app_constants.sh
+++ app_constants.sh
@@ -1,3 +1,3 @@
-env_script_location=https://raw.githubusercontent.com/fabric8-hdd/hypothesis-core-image/master/cico_utils/setup_env.sh
-APP_NAME=openshift-hdd-manager-api
+env_script_location=https://raw.githubusercontent.com/fabric8-hdd/fabric8-hypothesis-core-image/master/cico_utils/setup_env.sh
+APP_NAME=osio-hypothesis-manager-api
 APP_PORT=$(grep "HDD_MANAGER_API_PORT" openshift/supervisor.env | cut -d'=' -f 2)
