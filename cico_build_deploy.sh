83b0097c50dfab19f4cc904e71f370e91002ee76
anmolbabu <anmolbudugutta@gmail.com>
2018-02-19 03:39:48 +0530

    Pass app reg name
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -16,5 +16,5 @@ install_docker
 load_jenkins_vars
 docker_login
 
-BUILD_ARGS=$( format_build_args APP_PORT=${APP_PORT} APP_NAME=${APP_NAME} )
+BUILD_ARGS=$( format_build_args APP_PORT=${APP_PORT} APP_NAME=${APP_NAME} APP_REGISTRY=${DEFAULT_PULL_REGISTRY} )
 build_push_images -repo ${APP_NAME} -build-args "${BUILD_ARGS}" -app-version ${APP_VERSION} -test "yes" -docker-file Dockerfile.app -app-version ${APP_VERSION} -docker-file-test "Dockerfile.tests" -port ${APP_PORT}
