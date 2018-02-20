96a6e4fc584be656558b24fd1551ddac84700a00
anmolbabu <anmolbudugutta@gmail.com>
2018-02-20 16:01:44 +0530

    Add build machine os
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- app_constants.sh
+++ app_constants.sh
@@ -1,3 +1,7 @@
+set -a
 env_script_location=https://raw.githubusercontent.com/fabric8-hdd/fabric8-hypothesis-core-image/master/cico_utils/setup_env.sh
 APP_NAME=osio-hypothesis-manager-api
 APP_PORT=$(grep "HDD_MANAGER_API_PORT" openshift/supervisor.env | cut -d'=' -f 2)
+BUILD_MACHINE_OS=rhel
+os_prep_script_location=https://raw.githubusercontent.com/fabric8-hdd/fabric8-hypothesis-core-image/master/cico_utils/prerequisites/${BUILD_MACHINE_OS}.sh
+set +a
